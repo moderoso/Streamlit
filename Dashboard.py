01b9f7c7dde678453f5ca68d16d831f81e16d6fb
“Volmir” <moderoso@gmail.com>
2024-11-24 00:06:25 -0300

    Novos requirements_2

--- Dashboard.py
+++ Dashboard.py
@@ -41,11 +41,10 @@ miro_url = 'https://miro.com/app/live-embed/uXjVN1YW9H4=/?moveToViewport=-1349,-
 st.markdown(f'<iframe width="80%" height="600" src="{miro_url}" frameborder="0" scrolling="no" allow="fullscreen; clipboard-read; clipboard-write" allowfullscreen></iframe>', unsafe_allow_html=True)
 
 # Links
-st.markdown('## Links Úteis')
-st.markdown('##### Repositório do projeto')
+#st.markdown('## Links Úteis')
+st.markdown('##### Fontes de dados')
 st.markdown('[A história do petróleo no Brasil - Foi acessado em 15 novembro 2024](https://www.gov.br/anp/pt-br/acesso-a-informacao/institucional/historia-petroleo-brasil)')
 st.markdown('[Qual é a origem do petróleo? - Foi acessado em 15 novembro 2024](https://www.bbc.com/portuguese/articles/cnk0e0yydelo)')
-#st.markdown('##### Fontes de dados')
 st.markdown('[OPEC - Foi acessado em 15 novembro 2024]( https://www.opec.org/opec_web/en/index.htm)')
 st.markdown('[OpenAI. “O Chat GPT é uma ferramenta de processamento de linguagem natural orientada por IA, que possibilita conversas semelhantes às humanas com o chatbot - Foi acessado em 15 novembro 2024](https://openai.com/blog/chat-gpt-3-launch/)')
 #st.markdown('##### Fontes informacionais')
@@ -53,7 +52,7 @@ st.markdown('[Geopolítica do Petróleo - Foi acessado em 20 novembro 2024](http
 st.markdown('[Nos Bastidores da Terra: Geóloga Explica a Formação do Petróleo - Foi acessado em 20 novembro 2024](https://super.abril.com.br/coluna/deriva-continental/nos-bastidores-da-terra-geologa-explica-a-formacao-do-petroleo#google_vignette)')
 st.markdown('[Organização dos Países Exportadores de Petróleo - Foi acessado em 20 novembro 2024 ](https://pt.wikipedia.org/wiki/Organiza%C3%A7%C3%A3o_dos_Pa%C3%ADses_Exportadores_de_Petr%C3%B3leo)')
 st.markdown('[Agência Internacional de Energia - Foi acessado em 20 novembro 2024](https://pt.wikipedia.org/wiki/Ag%C3%AAncia_Internacional_de_Energia)')  
-st.markdown('[Oito motivos para a queda do preço do  petróleo - Foi acessado em 20 novembro 2024](https://www.dw.com/pt-br/oito-motivos-para-a-queda-do-pre%C3%A7o-do-petr%C3%B3leo/a-19051686)') 
+st.markdown('[Oito motivos para a queda do preço do  petróleo] Link acessado em 20 novembro 2024(https://www.dw.com/pt-br/oito-motivos-para-a-queda-do-pre%C3%A7o-do-petr%C3%B3leo/a-19051686)') 
 
 # Equipe do projeto
 st.markdown('## Equipe FIAP')
