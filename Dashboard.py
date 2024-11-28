6834b21532e9ce059583a83cef16d68c36ae58ea
“Volmir” <moderoso@gmail.com>
2024-11-27 22:36:03 -0300

    testes import dados

--- Dashboard.py
+++ Dashboard.py
@@ -20,30 +20,5 @@ coluna = 'Preco'
 dados = webscraping(url,coluna)
 
 # Construção dos dataframes 
-dados_sem_dataindex = dados.reset_index()
-dados_sem_dataindex['Ano'] = dados_sem_dataindex['Data'].dt.year.astype(int)
-
-media_mensal_anos = dados.groupby(pd.Grouper(freq= 'M'))[['Preco']].mean().reset_index() #Agrupando os dados por mês
-media_mensal_anos['Ano'] = media_mensal_anos['Data'].dt.year.astype(int)
-media_mensal_anos['Mes'] = media_mensal_anos['Data'].dt.month_name()
-
-picos_preco = media_mensal_anos.set_index('Data').sort_values('Preco', ascending=False).head(10)
-
-vales_preco = media_mensal_anos.set_index('Data').sort_values('Preco', ascending=True).head(10)
-
-# Construção dos gráficos          
-fig_picos_preco = px.bar(picos_preco,
-                        x = picos_preco['Mes'].astype(str) + '/' + picos_preco['Ano'].astype(str),
-                        y = 'Preco',
-                        text_auto=True,
-                        title= f'Top 10 meses com média de preços mais altos')
-fig_picos_preco.update_xaxes(type='category')
-fig_picos_preco.update_layout(xaxis_title= 'Data', yaxis_title = 'Preço (US$)')
-
-fig_vales_preco = px.bar(vales_preco,
-                        x = vales_preco['Mes'].astype(str) + '/' + vales_preco['Ano'].astype(str),
-                        y = 'Preco',
-                        text_auto=True,
-                        title= f'Top 10 meses com média de preços mais baixos')
-fig_vales_preco.update_xaxes(type='category')
-fig_vales_preco.update_layout(xaxis_title= 'Data', yaxis_title = 'Preço (US$)')
+df_dolar = pd.read_csv('dolar.csv', encoding = "ISO-8859-1", sep=";")
+df_dolar.head()
