0c7b2152683732e1d26aa853aa5ce9075bb3be47
“Volmir” <moderoso@gmail.com>
2024-11-28 23:55:53 -0300

    Update Dash

--- Dashboard.py
+++ Dashboard.py
@@ -36,12 +36,12 @@ if st.checkbox('Show dataframe'):
 plot = sns.lineplot(x='Data', y='Dolar Comercia (R$)', data=df_dolar)
 
 # Customize the plot (optional)
-plt.xlabel("Date")
-plt.ylabel("Dollar Commercial (R$)")
-plt.title("Dollar Commercial Rate Over Time")
+plot.xlabel("Date")
+plot.ylabel("Dollar Commercial (R$)")
+plot.title("Dollar Commercial Rate Over Time")
 ax2=ax.twinx()
 ax2.plot(np.arange(len(rides_number)), rides_number, color ='red', label="# rides")
-plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
+plot.xticks(rotation=45)  # Rotate x-axis labels for better readability
 
 st.pyplot(plot.fig)
 
