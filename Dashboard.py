364a9e748b45a9c07bebc5e62ec149201c74a2d2
“Volmir” <moderoso@gmail.com>
2024-11-28 23:22:34 -0300

    Update Dash

--- Dashboard.py
+++ Dashboard.py
@@ -32,63 +32,21 @@ if st.checkbox('Show dataframe'):
 	st.write(df_dolar)
 	
 
+# Create the plot
+sns.lineplot(x='Data', y='Dolar Comercia (R$)', data=df_dolar)
+
+# Customize the plot (optional)
+plt.xlabel("Date")
+plt.ylabel("Dollar Commercial (R$)")
+plt.title("Dollar Commercial Rate Over Time")
+plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
+
+# Show the plot
+plt.show()
+
+st.pyplot(plt.gcf())
 
 
-# Construção dos dataframes 
-dados_sem_dataindex = df_dolar
-dados_sem_dataindex['Ano'] = dados_sem_dataindex['Data'].dt.year.astype(int)
-
-media_mensal_anos = dados.groupby(pd.Grouper(freq= 'M'))[['Preco']].mean().reset_index() #Agrupando os dados por mês
-media_mensal_anos['Ano'] = media_mensal_anos['Data'].dt.year.astype(int)
-media_mensal_anos['Mes'] = media_mensal_anos['Data'].dt.month_name()
-
-picos_preco = media_mensal_anos.set_index('Data').sort_values('Preco', ascending=False).head(10)
-
-vales_preco = media_mensal_anos.set_index('Data').sort_values('Preco', ascending=True).head(10)
-
-
-# Construção dos gráficos          
-fig_picos_preco = px.bar(picos_preco,
-                        x = picos_preco['Mes'].astype(str) + '/' + picos_preco['Ano'].astype(str),
-                        y = 'Preco',
-                        text_auto=True,
-                        title= f'Top 10 meses com média de preços mais altos')
-fig_picos_preco.update_xaxes(type='category')
-fig_picos_preco.update_layout(xaxis_title= 'Data', yaxis_title = 'Preço (US$)')
-
-fig_vales_preco = px.bar(vales_preco,
-                        x = vales_preco['Mes'].astype(str) + '/' + vales_preco['Ano'].astype(str),
-                        y = 'Preco',
-                        text_auto=True,
-                        title= f'Top 10 meses com média de preços mais baixos')
-fig_vales_preco.update_xaxes(type='category')
-fig_vales_preco.update_layout(xaxis_title= 'Data', yaxis_title = 'Preço (US$)')
-
-
-# Visualização dos visuais no Streamlit
-## Cartões
-cor_estilizada = 'color: #0145AC;'
-fonte_negrito = 'font-weight: bold;'
-
-col1, col2, col3, col4 = st.columns(4)
-with col1:
-    metrica1 = dados.index.max().strftime('%d/%m/%Y')
-    st.markdown(f"<h2 style='{cor_estilizada}'>{metrica1}</h2> <span style='{fonte_negrito}'> Dados atualizados até </span>", unsafe_allow_html=True)
-    #st.metric('Dados atualizados até:', value=dados.index.max().strftime('%d/%m/%Y')) 
-with col2: 
-    metrica2 = dados.index.min().strftime('%d/%m/%Y')
-    st.markdown(f"<h2 style='{cor_estilizada}'> {metrica2} </h2> <span style='{fonte_negrito}'> Dados monitorados desde</span> ", unsafe_allow_html=True) 
-with col3:
-    metrica3 = dados['Preco'].min()
-    data_metrica3 = dados[dados['Preco']==dados['Preco'].min()].index
-    st.markdown(f"<h2 style='{cor_estilizada}'> US$ {metrica3:.2f} </h2> <span style='{fonte_negrito}'> Menor preço histórico <br> (atingido em  {data_metrica3[0].strftime('%d/%m/%Y')})</span> ", unsafe_allow_html=True) 
-    #st.metric('Menor preço histórico:', value=dados['Preco'].min().round(2))
-with col4:
-    metrica4 = dados['Preco'].max()
-    data_metrica4 = dados[dados['Preco']==dados['Preco'].max()].index
-    st.markdown(f"<h2 style='{cor_estilizada}'> US$ {metrica4:.2f} </h2> <span style='{fonte_negrito}'> Maior preço histórico <br> (atingido em  {data_metrica4[0].strftime('%d/%m/%Y')})</span> ", unsafe_allow_html=True)
-
-st.markdown("<div style='height: 40px;'></div>", unsafe_allow_html=True)
 
  # Select columns for the chart
 #    x_column = st.selectbox("Select X-axis column", df_dolar.columns)
