e8c70d14afd22550e64ec8d7dd3b53949a714b56
“Volmir” <moderoso@gmail.com>
2024-11-30 17:35:39 -0300

    Sobre

--- pages/Sobre.py
+++ pages/Sobre.py
@@ -1,24 +1,3 @@
-# Para realizar o WebScraping
-from bs4 import BeautifulSoup
-from prophet import Prophet
-from prophet.plot import plot_plotly, plot_components_plotly
-
-import requests
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import statsmodels.api as sm
-
-from prophet import Prophet
-from sklearn.model_selection import TimeSeriesSplit
-from sklearn.metrics import mean_absolute_error
-from sklearn.preprocessing import MinMaxScaler
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense, Dropout
-from statsmodels.tsa.arima.model import ARIMA
-
-
 # Importação da biblioteca streamlit
 import streamlit as st
 
