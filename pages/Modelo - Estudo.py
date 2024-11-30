20cc08b1d0e1072cfae1d6b9d5348ee0e715509b
“Volmir” <moderoso@gmail.com>
2024-11-30 17:56:15 -0300

    Modelo Estudo

--- pages/Modelo - Estudo.py
+++ pages/Modelo - Estudo.py
@@ -69,6 +69,14 @@ st.markdown('<p style="text-align: justify;"> Uma ferramenta poderosa para previ
 # Webscraping dos dados de petróleo
 url = 'http://www.ipeadata.gov.br/ExibeSerie.aspx?module=m&serid=1650971490&oper=view'
 
+# Construção dos dataframes 
+df_dolar = pd.read_csv('Valor_Dolar.csv', encoding = "ISO-8859-1", sep=";")
+df_dolar.head()
+
+
+df_petroleo = importacao_dados_previsao(url)
+df_petroleo.head()
+
 
 
 
