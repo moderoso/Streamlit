f77b9b8da7c02935f78502bb92cf1b10eacb68d5
“Volmir” <moderoso@gmail.com>
2024-11-30 14:09:14 -0300

    Modelo - Estudo

--- pages/Modelo - Estudo.py
+++ pages/Modelo - Estudo.py
@@ -17,9 +17,7 @@ st.markdown('<p style="text-align: justify;"><span style="font-weight: bold">Res
 st.markdown('<p style="text-align: justify;">Esses fatores combinados geraram um desequilíbrio entre oferta e demanda, pressionando os preços para baixo ao longo desse período.</p>', unsafe_allow_html = True)
 
 
-st.image(
-petroleo_mundo.png, width = 400
-)
+st.image("petroleo_mundo.png", width = 400)
 #st.markdown("[![Click Me](app/static/petroleo_mundo.png)](https://streamlit.io)")
 #st.image('petroleo_mundo.jpg', caption='Petroleo no Mundo', width=60)
 #st.markdown('<h1 style="float: left;">Petroleo no Mundo</h1><img style="float: right;" src="petroleo_mundo.jpg" />', unsafe_allow_html=True)
